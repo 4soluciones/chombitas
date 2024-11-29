ff33bb4a1a8daceee32ca066802d68ac563dbbb5
Jhon <jho5lp5@gmail.com>
2024-11-29 12:26:42 -0500

    cambios

--- apps/sales/views.py
+++ apps/sales/views.py
@@ -3236,8 +3236,8 @@ def get_order_detail_for_pay(request):
         cash_flow_of_distributions_with_deposits_set = None
         if order_obj.distribution_mobil is not None:
             cash_flow_of_distributions_with_deposits_set = CashFlow.objects.filter(
-                # distribution_mobil__truck=order_obj.distribution_mobil.truck,
                 distribution_mobil__isnull=False,
+                distribution_mobil__truck=order_obj.distribution_mobil.truck,
                 distribution_mobil__truck__subsidiary=subsidiary_obj,
                 type__in=['D', 'E']
             ).annotate(
